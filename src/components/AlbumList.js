673e0d2a44a2ca3539f8808528475c03c9e00aea
Ben Slabbert <ben@hex.co.za>
2017-12-26 22:30:45 +0200

    adding album list component

---
+++ src/components/AlbumList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const AlbumList = () => {
+    return (
+        <View>
+            <Text>
+                album list2
+            </Text>
+        </View>
+    )
+};
+
+export default AlbumList;
