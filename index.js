2b3daf729bede4ea9ee870faa69645b6777430f7
Ben Slabbert <ben@hex.co.za>
2017-12-26 22:19:44 +0200

    adding header styles

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import { AppRegistry } from 'react-native';
 import Header from './src/components/Header';
 
 const App = () => (
-    <Header/>
+    <Header title='Albums'/>
 );
 
 // must render at least one component
