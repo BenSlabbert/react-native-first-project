ce5ce59a8db475f5bb7a49f7e70dd0b613510401
Ben Slabbert <ben@hex.co.za>
2017-12-26 22:05:17 +0200

    adding styles to view tag

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,23 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 
 const Header = () => {
 
-    const { textStyle } = styles;
+    const { viewStyle, textStyle } = styles;
 
-    return <Text style={textStyle}>Header</Text>
+    return (
+        <View style={viewStyle}>
+            <Text style={textStyle}>Header</Text>
+        </View>
+    );
 };
 
 const styles = {
+
+    viewStyle: {
+        backgroundColor: '#F8F8F8'
+    },
+
     textStyle: {
         fontSize: 20
     }
