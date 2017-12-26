c054679fe7722bb87be20f61ce01cab8819009a1
Ben Slabbert <ben@hex.co.za>
2017-12-26 22:00:11 +0200

    adding styles to header

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
 import React from 'react';
-import { Text, AppRegistry } from 'react-native';
+import { AppRegistry } from 'react-native';
 
 import Header from './src/components/Header';
 
-const App = () => {
-    return (
-        <Text>
-            
-            <Header/>
-        </Text>
-    );
-};
+const App = () => (
+    <Header/>
+);
 
 // must render at least one component
 AppRegistry.registerComponent( 'FirstProject', () => App );
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,16 @@ import React from 'react';
 import { Text } from 'react-native';
 
 const Header = () => {
-    return <Text>Header</Text>
+
+    const { textStyle } = styles;
+
+    return <Text style={textStyle}>Header</Text>
+};
+
+const styles = {
+    textStyle: {
+        fontSize: 20
+    }
 };
 
 export default Header;
