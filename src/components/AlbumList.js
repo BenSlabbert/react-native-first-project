d87a0171a59a9698fb6c5937cf1131a07e0274b1
Ben Slabbert <ben@hex.co.za>
2017-12-26 22:33:09 +0200

    refactoring to class based component

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 
-const AlbumList = () => {
-    return (
-        <View>
-            <Text>
-                album list2
-            </Text>
-        </View>
-    )
-};
+class AlbumList extends Component {
+
+    constructor( props ) {
+        super( props );
+    }
+
+    render() {
+        return (
+            <View>
+                <Text>
+                    album list
+                </Text>
+            </View>
+        );
+    }
+}
 
 export default AlbumList;
