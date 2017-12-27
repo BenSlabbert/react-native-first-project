b550f742dc2a79d393cfdd02294581911e3c5a1d
Ben Slabbert <ben@hex.co.za>
2017-12-27 20:50:53 +0200

    adding images must specify the image dimensions for it to render

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,19 +1,43 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View, Image } from 'react-native';
 
 import CardSection from './CardSection';
 
 import Card from './Card';
 
-const AlbumDetail = ( props ) => {
+const AlbumDetail = ( { album } ) => {
+
+    const { title, artist, thumbnail_image } = album;
+    const { thumbnailStyle, headerContentStyle } = styles;
 
     return (
         <Card>
             <CardSection>
-                <Text>{props.album.title}</Text>
+                <View>
+                    <Image style={thumbnailStyle}
+                           source={{ uri: thumbnail_image }}
+                    />
+                </View>
+                <View style={headerContentStyle}>
+                    <Text>{title}</Text>
+                    <Text>{artist}</Text>
+                </View>
             </CardSection>
         </Card>
     );
+};
+
+const styles = {
+    headerContentStyle: {
+        flexDirection: 'column',
+        justifyContent: 'space-around'
+    },
+
+    // must specify the image sizes
+    thumbnailStyle: {
+        height: 50,
+        width: 50
+    }
 };
 
 export default AlbumDetail;
