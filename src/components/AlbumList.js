b159396fcae265e6612845d23f28ef2182dff852
Ben Slabbert <ben@hex.co.za>
2017-12-27 08:19:53 +0200

    adding axios to fetch data rendering list of albums

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+import axios from 'axios';
 
 class AlbumList extends Component {
+    state = { albums: [] };
 
-    constructor( props ) {
-        super( props );
+    componentWillMount() {
+        axios.get( 'https://rallycoding.herokuapp.com/api/music_albums' )
+            .then( response => this.setState( { albums: response.data } ) );
+    }
+
+    renderAlbumList() {
+        return this.state.albums.map( album => <Text key={album.title}>{album.title}</Text> );
     }
 
     render() {
         return (
             <View>
-                <Text>
-                    album list
-                </Text>
+                {this.renderAlbumList()}
             </View>
         );
     }
